bba956ff525ce42544b06e5eb803cace0c64cbd8
greenJian <879411990@qq.com>
2026-07-27 21:32:07 +0800

    fix: 修正仓库名为小写 for-beibei

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/For-beibei/',
+  base: '/for-beibei/',
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
